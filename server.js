e57cc5e37a06e2f762b884120eb8a3b68823b5d4
lunoos <pmahendar2373@gmail.com>
2020-06-24 22:12:43 +0530

    change

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)});
 
 
 
-app.listen(process.env.PROT || 3001, () => {
-	console.log(`app is running on port ${process.env.PORT}`);
+app.listen(3001, () => {
+	console.log('app is running on port 3001');
 })
